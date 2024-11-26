c10d7c3369c89360ab76876d568cc811abced852
G00425238@atu.ie <g00425238@atu,ie>
2024-11-26 11:36:38 +0000

    Created Microservice

---
+++ src/test/java/ie/atu/carsaleapp_customer/CarSaleAppCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.carsaleapp_customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSaleAppCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
