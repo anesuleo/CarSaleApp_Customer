500a8c6ebac154147cfac5327cafad32dc13b6b4
Nokuvimba <G00423951@atu.ie>
2024-12-17 19:35:21 +0000

    Customer can now access cars using openFeign.

---
+++ src/main/java/ie/atu/carsaleapp_customer/CustomerClient.java
@@ -0,0 +1,19 @@
+package ie.atu.carsaleapp_customer;
+
+
+
+import jakarta.validation.Valid;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+import java.util.List;
+
+@FeignClient(name="car",url ="http://localhost:8080")
+public interface CustomerClient {
+    @GetMapping("cars/allcars")
+     List<Car> getAllCars() ;
+
+}
